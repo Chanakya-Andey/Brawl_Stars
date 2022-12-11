af97c26abd06441c7b2f21e6c19fe834837de8cb
Chanakya Andey <81629418+Chanakya-Andey@users.noreply.github.com>
2022-12-11 22:38:24 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,34 @@
+## THE FOLLOWING IS CONFIGURATION FOR BRAWL STARS, DO NOT COMMIT THIS FILE ANYWHERE
+
+# Set it to false if you don't want to send the reports via mail
+Mailer    = True
+
+# Set it to false if you don't want to log the change in your brawler stats
+Logs      = True
+
+# Set it to false if you don't want to overwrite brawler stats in your local machine
+Overwrite = True
+
+# Set it to 0 (type: int) if you want to generate the charts without updating the brawler stats
+# Set it to Manual if you want to generate the charts with manually updated brawler stats
+# Set it to anything else if you want to generate the charts with automatically updated brawler stats
+Update    = 'API' # [0, 'Manual', 'API']
+
+# Set it to Power to view the brawlers in the order of power
+Sorting   = 'Trophies' # ['Trophies', 'Power']
+
+# Select the dpi of charts, recommended - 100
+dpi       = 100
+
+## Please follow the steps to enter the string values
+
+token = ""
+# The API wouldn't work unless you provide the functioning key
+# 1. Create a key at https://developer.brawlstars.com/#/account
+# 2. Copy the token from the key section and paste it here.
+
+sender_ = "" # Enter the sender's mail id if the mailer is set to True
+
+receiver = "" # Enter the receiver's mail id if the mailer is set to True
+
+player_tag = "#8RQCY8UPY" # Enter your player tag gere. Ex: #8RQCY8UPY"
